0a5f64ebaabeb5b6694f3faa954708548f1d7fdf
ezzinemehdi <ezzinevil@gmail.com>
2025-05-06 16:13:28 +0100

    h

--- srcs/requirements/wordpress/tools/setupWordpress.sh
+++ srcs/requirements/wordpress/tools/setupWordpress.sh
@@ -1,29 +1,35 @@
 #!/bin/bash
+# sleep 10
+if [ ! -f wp-config.php ]; then
+	wp core download --allow-root
 
-wp core download --allow-root
+	wp config create \
+	--dbname="$MYSQL_DATABASE" \
+	--dbuser="$MYSQL_USER" \
+	--dbpass="$MYSQL_PASSWORD" \
+	--dbhost="mariadb:3306" \
+	--allow-root
 
-wp core install \
---url="http://127.0.0.1:8000/" \
---title="Example Site" \
---admin_user=$MYSQL_USER \
---admin_password=$MYSQL_PASSWORD \
---admin_email=admin@example.com \
---skip-email \
---allow-root
+	wp core install \
+	--url="http://127.0.0.1:8000/" \
+	--title="Example Site" \
+	--admin_user=$MYSQL_USER \
+	--admin_password=$MYSQL_PASSWORD \
+	--admin_email=admin@example.com \
+	--skip-email \
+	--allow-root
 
-wp config create \
---dbname="$MYSQL_DATABASE" \
---dbuser="$MYSQL_USER" \
---dbpass="$MYSQL_PASSWORD" \
---dbhost="mariadb:3306" \
---allow-root
-
-if ! wp user exists user --allow-root; then
-  wp user create user example@example.com \
+	wp user create user example@example.com \
     --user_pass=123456 \
     --allow-root
-else
-  echo "User 'user' already exists; skipping creation."
 fi
+
+# if ! wp user exists user --allow-root; then
+#   wp user create user example@example.com \
+#     --user_pass=123456 \
+#     --allow-root
+# else
+#   echo "User 'user' already exists; skipping creation."
+# fi
 
 /usr/sbin/php-fpm8.2 -F
