21279b4a2ea2fdf708fb40932130467ff9c61e78
ezzinemehdi <ezzinevil@gmail.com>
2025-06-04 13:49:01 +0100

    backup

--- srcs/requirements/bonus/ftp/tools/script.sh
+++ srcs/requirements/bonus/ftp/tools/script.sh
@@ -9,9 +9,9 @@ echo "pasv_min_port=4000" >> /etc/vsftpd.conf
 echo "pasv_max_port=4005" >> /etc/vsftpd.conf
 echo "file_open_mode=0777" >> /etc/vsftpd.conf
 
-adduser $FTP_USER --home /var/www/wordpress
+adduser $FTP_USER --home /var/www/html/wordpress
 echo "$FTP_USER:$FTP_PASSWORD" | chpasswd
 
-chown -R "$FTP_USER:$FTP_USER" /var/www/wordpress
+chown -R "$FTP_USER:$FTP_USER" /var/www/html/wordpress
 
 vsftpd /etc/vsftpd.conf
